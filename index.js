8a98462c4e55d532a23be8f71609af9051d5af89
Sheena Puthan <sheena.puthan@gmail.com>
2024-08-22 14:02:56 +0300

    notes backend

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+const cors = require('cors')
+
+app.use(cors())
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
+
+app.use(requestLogger)
+let notes = [
+    {
+        id: "1",
+        content: "HTML is easy",
+        important: true
+    },
+    {
+        id: "2",
+        content: "Browser can execute only JavaScript",
+        important: false
+    },
+    {
+        id: "3",
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World! </h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = request.params.id
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = request.params.id
+    notes = notes.filter(note => note.id !== id)
+  
+    response.status(204).end()
+  })
+
+  app.post('/api/notes', (request, response) => {
+    const note = request.body
+    console.log(note)
+    note.id = String(maxId + 1)
+
+    notes = notes.concat(note)
+    
+    response.json(note)
+  })
+
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  app.use(unknownEndpoint)
+
+const PORT = process.env.port || 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
