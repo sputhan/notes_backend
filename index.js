15598fc480e43c296091923e758c0607bae7935a
Sheena Puthan <sheena.puthan@gmail.com>
2024-08-22 17:08:49 +0300

    fix post error

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const requestLogger = (request, response, next) => {
     console.log('Body:  ', request.body)
     console.log('---')
     next()
-  }
+}
 
 app.use(requestLogger)
 
@@ -56,26 +56,44 @@ app.get('/api/notes/:id', (request, response) => {
 app.delete('/api/notes/:id', (request, response) => {
     const id = request.params.id
     notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
-  })
 
-  app.post('/api/notes', (request, response) => {
-    const note = request.body
-    console.log(note)
-    note.id = String(maxId + 1)
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => Number(n.id)))
+        : 0
+    return String(maxId + 1)
+}
+
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: Boolean(body.important) || false,
+        id: generateId(),
+    }
 
     notes = notes.concat(note)
-    
+
     response.json(note)
-  })
+})
 
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
-  }
-  
-  app.use(unknownEndpoint)
+}
+
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT)
